a8950bffca9d49ad176d14706646f26f9680a81a
Michael Sexton <mick.sexton@gmail.com>
2015-10-31 10:25:29 +1100

    Added admin fields

--- src/main/java/au/gov/ga/ozmin/model/MineralResource.java
+++ src/main/java/au/gov/ga/ozmin/model/MineralResource.java
@@ -13,15 +13,19 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+
 @Entity
-@Table(schema="MGD",name="RESOURCES")
+@Table(schema = "MGD", name = "RESOURCES")
 public class MineralResource {
 
 	@Id
-	@Column(name="RESOURCENO") 
+	@Column(name = "RESOURCENO")
 	private Long id;
-	
-	@Column(name="RECORDDATE")
+
+	@Column(name = "RECORDDATE")
 	private Date recordDate;
 
 	@ManyToOne
@@ -52,13 +56,36 @@ public class MineralResource {
 	@Column(name = "OTHER")
 	private Double other;
 
+	// CDM Admin fields
+
+	@Column(name = "ACCESS_CODE")
+	private String accessCode;
+
+	@Column(name = "QA_STATUS_CODE")
+	private String qaStatus;
+
+	@Column(name = "QABY")
+	private String qaBy;
+
+	@Column(name = "QADATE")
+	private Date qaDate;
+
+	@Column(name = "ENTEREDBY")
+	private String enteredBy;
+
+	@Column(name = "ENTRYDATE")
+	private Date entryDate;
+
+	// Related models
+	
 	@ManyToOne
+	@Fetch(FetchMode.JOIN)
 	@JoinColumn(name = "ENO")
 	private MineralisedZone mineralisedZone;
 
-	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "mineralResource")
-    private List<ResourceGrade> resourceGrades;
-	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "mineralResource")
+	private List<ResourceGrade> resourceGrades;
+
 	public List<ResourceGrade> getResourceGrades() {
 		return resourceGrades;
 	}
@@ -162,4 +189,53 @@ public class MineralResource {
 	public void setMineralisedZone(MineralisedZone mineralisedZone) {
 		this.mineralisedZone = mineralisedZone;
 	}
+
+	public String getAccessCode() {
+		return accessCode;
+	}
+
+	public String getQaStatus() {
+		return qaStatus;
+	}
+
+	public String getQaBy() {
+		return qaBy;
+	}
+
+	public Date getQaDate() {
+		return qaDate;
+	}
+
+	public String getEnteredBy() {
+		return enteredBy;
+	}
+
+	public Date getEntryDate() {
+		return entryDate;
+	}
+
+	public void setAccessCode(String accessCode) {
+		this.accessCode = accessCode;
+	}
+
+	public void setQaStatus(String qaStatus) {
+		this.qaStatus = qaStatus;
+	}
+
+	public void setQaBy(String qaBy) {
+		this.qaBy = qaBy;
+	}
+
+	public void setQaDate(Date qaDate) {
+		this.qaDate = qaDate;
+	}
+
+	public void setEnteredBy(String enteredBy) {
+		this.enteredBy = enteredBy;
+	}
+
+	public void setEntryDate(Date entryDate) {
+		this.entryDate = entryDate;
+	}
+
 }
