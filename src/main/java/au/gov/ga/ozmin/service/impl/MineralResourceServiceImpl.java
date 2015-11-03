ee66fe2842e977bf2a17b504970a4d0d6cfce320
Michael Sexton <mick.sexton@gmail.com>
2015-11-03 16:44:56 +1100

    Added getMineralResourceById to service

--- src/main/java/au/gov/ga/ozmin/service/impl/MineralResourceServiceImpl.java
+++ src/main/java/au/gov/ga/ozmin/service/impl/MineralResourceServiceImpl.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import au.gov.ga.ozmin.model.MineralResource;
 import au.gov.ga.ozmin.repository.MineralResourceRepository;
@@ -27,9 +28,11 @@ public class MineralResourceServiceImpl implements MineralResourceService {
 	}
 
 	@Override
+	@Transactional
 	public MineralResource getMineralResourceById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		MineralResource mineralResource= this.mineralResourceRepository.findOne(id);
+
+		return mineralResource;
 	}
 
 	@Override
