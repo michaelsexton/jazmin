9da30e1cdf762a41e5d97d0987aa376c0851bc48
Michael Sexton <mick.sexton@gmail.com>
2015-11-03 13:25:21 +1100

    Fixed web init

--- src/main/java/au/gov/ga/ozmin/config/WebInit.java
+++ src/main/java/au/gov/ga/ozmin/config/WebInit.java
@@ -1,32 +1,32 @@
 package au.gov.ga.ozmin.config;
 
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 public class WebInit implements WebApplicationInitializer {
 
 	@Override
-	public void onStartup(ServletContext servletContext) throws ServletException {
-		WebApplicationContext context = appContext();
+	public void onStartup(ServletContext container) {
+		// Create the 'root' Spring application context
+		AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+		rootContext.register(AppConfig.class);
 
-		servletContext.addListener(new ContextLoaderListener(context));
-		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
-        dispatcher.setLoadOnStartup(1);
-        dispatcher.addMapping("/*");
+		// Manage the lifecycle of the root application context
+		container.addListener(new ContextLoaderListener(rootContext));
 
+		// Create the dispatcher servlet's Spring application context
+		AnnotationConfigWebApplicationContext dispatcherContext = new AnnotationConfigWebApplicationContext();
+		dispatcherContext.register(WebMvcConfig.class);
+
+		// Register and map the dispatcher servlet
+		ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcher",
+				new DispatcherServlet(dispatcherContext));
+		dispatcher.setLoadOnStartup(1);
+		dispatcher.addMapping("/");
 	}
-
-	private AnnotationConfigWebApplicationContext appContext() {
-		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-		context.setConfigLocation("au.gov.ga.ozmin.config");
-		return context;
-	}
-
 }
