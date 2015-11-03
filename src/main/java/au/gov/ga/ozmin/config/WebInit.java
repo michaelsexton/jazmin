f17372cd946e2eed619f78ce900e2626686a1864
Michael Sexton <mick.sexton@gmail.com>
2015-11-03 13:25:53 +1100

    Removed comments

--- src/main/java/au/gov/ga/ozmin/config/WebInit.java
+++ src/main/java/au/gov/ga/ozmin/config/WebInit.java
@@ -12,18 +12,18 @@ public class WebInit implements WebApplicationInitializer {
 
 	@Override
 	public void onStartup(ServletContext container) {
-		// Create the 'root' Spring application context
+
 		AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
 		rootContext.register(AppConfig.class);
 
-		// Manage the lifecycle of the root application context
+
 		container.addListener(new ContextLoaderListener(rootContext));
 
-		// Create the dispatcher servlet's Spring application context
+
 		AnnotationConfigWebApplicationContext dispatcherContext = new AnnotationConfigWebApplicationContext();
 		dispatcherContext.register(WebMvcConfig.class);
 
-		// Register and map the dispatcher servlet
+
 		ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcher",
 				new DispatcherServlet(dispatcherContext));
 		dispatcher.setLoadOnStartup(1);
