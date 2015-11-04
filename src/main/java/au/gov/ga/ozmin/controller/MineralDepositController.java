d865628731be634025927ad5ff351b6928226582
Michael Sexton <mick.sexton@gmail.com>
2015-11-05 09:24:03 +1100

    Reordered some things

--- src/main/java/au/gov/ga/ozmin/controller/MineralDepositController.java
+++ src/main/java/au/gov/ga/ozmin/controller/MineralDepositController.java
@@ -1,19 +1,17 @@
 package au.gov.ga.ozmin.controller;
 
-import au.gov.ga.ozmin.model.MineralDeposit;
-import au.gov.ga.ozmin.service.MineralDepositService;
-import au.gov.ga.ozmin.util.Paginator;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import au.gov.ga.ozmin.model.MineralDeposit;
+import au.gov.ga.ozmin.service.MineralDepositService;
+import au.gov.ga.ozmin.util.Paginator;
 
 
 /**
