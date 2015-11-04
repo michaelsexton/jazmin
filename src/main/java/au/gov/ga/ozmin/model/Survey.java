a7824d6eea03e63fbdb7605fb88be8b58663c252
Michael Sexton <mick.sexton@gmail.com>
2015-11-04 11:43:33 +1100

    Added surveys domain model

---
+++ src/main/java/au/gov/ga/ozmin/model/Survey.java
@@ -0,0 +1,13 @@
+package au.gov.ga.ozmin.model;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.SecondaryTable;
+
+@Entity
+@DiscriminatorValue("SURVEY")
+@SecondaryTable(schema="A", name="SURVEYS",pkJoinColumns=@PrimaryKeyJoinColumn(name="ENO"))
+public class Survey extends SpatialEntity {
+
+}
