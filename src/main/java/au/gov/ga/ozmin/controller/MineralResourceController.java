a88523664ca73d3a897944651d06ed0a878d1d1a
Michael Sexton <mick.sexton@gmail.com>
2015-11-04 11:12:43 +1100

    Removed unnecessary imports

--- src/main/java/au/gov/ga/ozmin/controller/MineralResourceController.java
+++ src/main/java/au/gov/ga/ozmin/controller/MineralResourceController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import au.gov.ga.ozmin.model.MineralDeposit;
 import au.gov.ga.ozmin.model.MineralResource;
 import au.gov.ga.ozmin.service.MineralResourceService;
 import au.gov.ga.ozmin.util.Paginator;
