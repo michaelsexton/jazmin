4ee30087371fa06e70d47c203be7437763f1a1cd
Michael Sexton <mick.sexton@gmail.com>
2015-11-04 12:06:55 +1100

    Investigated attribute override

--- src/main/java/au/gov/ga/ozmin/model/Province.java
+++ src/main/java/au/gov/ga/ozmin/model/Province.java
@@ -1,7 +1,5 @@
 package au.gov.ga.ozmin.model;
 
-import javax.persistence.AttributeOverride;
-import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.PrimaryKeyJoinColumn;
@@ -13,18 +11,6 @@ import javax.persistence.SecondaryTable;
 @SecondaryTable(schema="PROVS",name = "PROVINCES", pkJoinColumns = @PrimaryKeyJoinColumn(name="ENO"))
 public class Province extends SpatialEntity {
 	
-	@AttributeOverride(name="id", column = @Column(name = "PROVNO"))
-	Long id;
-
-	@Override
-	public Long getId() {
-		return id;
-	}
-
-	@Override
-	public void setId(Long id) {
-		this.id = id;
-	}
-
+	// TODO: Investigate Attribute override
 	
 }
