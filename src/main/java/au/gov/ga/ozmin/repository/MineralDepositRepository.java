d5f42cbeae951457abc9aba59ee01a3b9d0d8c37
Michael Sexton <mick.sexton@gmail.com>
2015-11-19 16:20:53 +1100

    Added query annotation for single page deposit view

--- src/main/java/au/gov/ga/ozmin/repository/MineralDepositRepository.java
+++ src/main/java/au/gov/ga/ozmin/repository/MineralDepositRepository.java
@@ -2,6 +2,8 @@ package au.gov.ga.ozmin.repository;
 
 
 import au.gov.ga.ozmin.model.MineralDeposit;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
@@ -9,4 +11,12 @@ import java.util.List;
 public interface MineralDepositRepository extends PagingAndSortingRepository<MineralDeposit, Long> {
 	List<MineralDeposit> findByName(String name);
 
+	@Query("select md from MineralDeposit as md "
+			+ "left join fetch md.mineralProject "
+			+ "left join fetch md.mineralisedZones as mz "
+			+ "left join fetch mz.mineralResources as mr "
+			+ "left join fetch md.mineralDepositCommodityOrders "
+			+ "left join fetch md.provinces "
+			+ "where md.id = ?1")
+	MineralDeposit findDepositWithAssociatedData(Long id);
 }
