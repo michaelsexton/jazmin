e043264a650bcadb957199aab0a72e6dbfef9d99
Michael Sexton <mick.sexton@gmail.com>
2015-11-25 21:13:02 +1100

    Cleaned up imports

--- src/main/java/au/gov/ga/ozmin/view/ResourceQualityCheckPdfView.java
+++ src/main/java/au/gov/ga/ozmin/view/ResourceQualityCheckPdfView.java
@@ -18,7 +18,6 @@ import com.lowagie.text.Document;
 import com.lowagie.text.Font;
 import com.lowagie.text.HeaderFooter;
 import com.lowagie.text.PageSize;
-import com.lowagie.text.Paragraph;
 import com.lowagie.text.Phrase;
 import com.lowagie.text.Rectangle;
 import com.lowagie.text.pdf.PdfPCell;
