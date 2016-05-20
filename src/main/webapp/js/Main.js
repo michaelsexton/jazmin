1e91054e7d7307ee6af4506c1cdc7c67179cec39
comtesacristain <mick.sexton@gmail.com>
2016-05-20 16:10:19 +1000

    Test of Extjs

--- src/main/webapp/js/Main.js
+++ src/main/webapp/js/Main.js
@@ -1,17 +1,49 @@
+Ext.Loader.setPath('ozmin', 'js/ozmin');
 Ext.application({
-    name   : 'MyApp',
-    theme: "theme-triton",
+  name : 'MineralDeposit',
+  theme : "theme-triton",
 
-    launch : function() {
+  launch : function() {
 
-       Ext.create('Ext.Panel', {
-            renderTo     : Ext.getBody(),
-            width        : 200,
-            height       : 150,
-            bodyPadding  : 5,
-            title        : 'Hello World',
-            html         : 'Hello <b>World</b>...'
-        });
+    Ext.define('MineralDeposit', {
+      extend : 'Ext.data.Model',
+      fields : [ 'id', 'name', 'operatingStatus', 'state', 'synonyms' ],
+    });
 
-    }
-});	
+    Ext.create('Ext.data.Store', {
+      storeId: 'mineralDepositsStore',
+      model : 'MineralDeposit',
+      proxy : {
+        type : 'ajax',
+        url : '/ozmin/mineralDeposits.json',
+        reader : {
+          type : 'json'
+          //rootProperty : 'users'
+        }
+      },
+      autoLoad : true
+    });
+
+    Ext.create('Ext.grid.Panel', {
+      title : 'Mineral Deposits',
+      store : Ext.data.StoreManager.lookup('mineralDepositsStore'),
+      columns : [ {
+        text : 'ENO',
+        dataIndex : 'id'
+      }, {
+        text : 'Name',
+        dataIndex : 'name',
+        flex : 1
+      }, {
+        text : 'Operating Status',
+        dataIndex : 'operatingStatus'
+      }, {
+        text : 'State',
+        dataIndex : 'state'
+      } ],
+      height : 300,
+      width : 600,
+      renderTo : Ext.getBody()
+    });
+  }
+});
