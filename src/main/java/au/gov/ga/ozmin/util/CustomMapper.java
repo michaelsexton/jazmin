3fdc964647f2139a5365b4c72c09730135a08e95
comtesacristain <mick.sexton@gmail.com>
2016-05-20 16:08:53 +1000

    Add custom mapper for JTS

---
+++ src/main/java/au/gov/ga/ozmin/util/CustomMapper.java
@@ -0,0 +1,16 @@
+package au.gov.ga.ozmin.util;
+
+import com.bedatadriven.jackson.datatype.jts.JtsModule;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class CustomMapper extends ObjectMapper {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8054717390969839744L;
+
+	public CustomMapper() {
+		super();
+		this.registerModule(new JtsModule());
+	}
+}
