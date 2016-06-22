95067903b321db995215886c3fe56930fbff1a25
comtesacristain <mick.sexton@gmail.com>
2016-06-22 16:09:20 +1000

    Remove useless code

--- src/main/java/au/gov/ga/ozmin/controller/ApplicationController.java
+++ src/main/java/au/gov/ga/ozmin/controller/ApplicationController.java
@@ -1,38 +1,15 @@
 package au.gov.ga.ozmin.controller;
 
-//import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
-//import au.gov.ga.ozmin.model.SpatialEntity;
-// TODO: Make Front page for Spatial Entity
 @Controller
 @RequestMapping(value = "/")
 public class ApplicationController {
 
 	@RequestMapping(value = "", method = RequestMethod.GET)
-	public String listEntities(Model model,
-			@RequestParam(value = "page", defaultValue = "1") Integer pageNumber) {
+	public String homePage() {
 		return "index";
-		// Page<Commodity> commodityPage =
-		// commodityService.listCommodities(pageNumber);
-		//
-		// int current = commodityPage.getNumber() + 1;
-		// int begin = Math.max(1, current - 5);
-		// int end = Math.min(begin + 10, commodityPage.getTotalPages());
-		//
-		// model.addAttribute("listCommodities", commodityPage);
-		// model.addAttribute("beginIndex", begin);
-		// model.addAttribute("endIndex", end);
-		// model.addAttribute("currentIndex", current);
-		//
-		// //model.addAttribute("listCommodities",
-		// this.commodityService.listCommodities());
-		//
-		//
-		// return "commodities/index";
 	}
 }
